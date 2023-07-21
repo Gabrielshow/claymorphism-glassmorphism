165eac93d74c3bf03fd67fbd583fd16bb1aaf4f0
Gabrielshow <adefelashogbanmu@gmail.com>
2023-07-21 02:03:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <p>
+        <span></span>design
+        <br />
+        trends
+        <br />
+        <i>2023</i>
+      </p>
+      <div></div>
+    </>
+  );
+}
+
+export default App
